0e3324faed64d0fefb092c2d39c61f8adcfdcc11
pm-raghunandan-gupta <raghunandan.gupta@pubmatic.com>
2015-08-09 18:56:31 +0530

    Swagger Functionality added.

--- src/main/java/com/loginext/test/services/CoordinateService.java
+++ src/main/java/com/loginext/test/services/CoordinateService.java
@@ -7,6 +7,10 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+
+@Api(value = "/coordinateServices", description = "REST Services for fetching Coordniates")
 @WebService(serviceName = "customerServices")
 @Produces({ MediaType.APPLICATION_JSON })
 public interface CoordinateService {
@@ -14,11 +18,13 @@ public interface CoordinateService {
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
     @Path("/coordinate/{id}")
+    @ApiOperation(value = "Get Coordinate", response = Object.class)
     public Object getCoordinate(@PathParam("id") Integer id);
 
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
     @Path("/coordinates")
+    @ApiOperation(value = "Get Coordinates", response = Object.class)
     public Object getAllCoordinates();
 
 }
